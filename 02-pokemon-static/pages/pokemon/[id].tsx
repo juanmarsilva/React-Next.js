69dc0596272d0d587523a9289d9d9cca00c80884
Juan Martin Silva <juanmartinsilva74@gmail.com>
2022-12-01 11:18:53 -0300

    get info about each pokemon

--- 02-pokemon-static/pages/pokemon/[id].tsx
+++ 02-pokemon-static/pages/pokemon/[id].tsx
@@ -1,26 +1,52 @@
 import React from 'react'
-import { Layout } from '../../components/layouts';
+import { NextPage, GetStaticProps, GetStaticPaths } from 'next';
 import { useRouter } from 'next/router';
+import { pokeApi } from '../../api';
+import { Pokemon } from '../../interfaces';
+import { Layout } from '../../components/layouts';
+
 
 interface Props {
-    pokemon: any;
+    pokemon: Pokemon;
 }
 
-const PokemonPage = () => {
+const PokemonPage: NextPage<Props> = ({ pokemon }) => {
 
     const router = useRouter();
 
-
-
     return (
         <Layout>
-            <h1>Hola wacho!</h1>
+            <h1> { pokemon.name } </h1>
         </Layout>
     )
 };
 
 
+export const getStaticPaths: GetStaticPaths = async (ctx) => {
 
+    const pokemon151 = [...Array(151)].map(( value, index ) => `${ index + 1 }`)
+    
+    return {
+        paths: pokemon151.map( id => ({
+            params: { id }
+        })),
+        fallback: false
+    };
+};
+
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  
+    const { id } = params as { id: string };
+    const { data } = await pokeApi<Pokemon>(`/pokemon/${ id }`);
+    
+    return {
+      props: {
+        pokemon: data
+      }
+    };
+};
+  
 
 
 export default PokemonPage;
