f8d9b8728be5e500301503010291484c087d0b2a
Juan Martin Silva <juanmartinsilva74@gmail.com>
2022-12-05 12:25:52 -0300

    add open graph meta tags

--- 02-pokemon-static/components/layouts/Layout.tsx
+++ 02-pokemon-static/components/layouts/Layout.tsx
@@ -7,16 +7,25 @@ interface Props {
     title?: string;
 };
 
+const origin = ( typeof window === 'undefined' ) ? '' : window.location.origin;
+
 export const Layout: FC<Props> = ({ children, title }) => {
 
+    
 
     return (
         <>
             <Head>
+
                 <title> { title || 'Pokemon App' } </title>
                 <meta name='author' content='Juan Martin Silva' />
                 <meta name='description' content={`Informacion sobre el pokemon ${ title }`} />
                 <meta name='keywords' content={`${ title }, pokemon, pokedex`} />
+
+                <meta property="og:title" content={`Informacion sobre ${ title }`} />
+                <meta property="og:description" content={`Esta es la pagina sobre ${ title }`} />
+                <meta property="og:image" content={`${origin}/img/banner.png`} />
+                
             </Head>
 
             <Navbar />
