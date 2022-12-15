4dfde8fbbf1c6441fce01a980663ddb54cf253ad
Juan Martin Silva <juanmartinsilva74@gmail.com>
2022-12-15 20:09:22 -0300

    server side rendering

---
+++ 03-open-jira/pages/entries/[id].tsx
@@ -0,0 +1,156 @@
+import { ChangeEvent, useState, useMemo, FC } from 'react';
+import { GetServerSideProps } from 'next'
+import { capitalize, Grid, Card, CardHeader, CardContent, TextField, CardActions, Button, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio, IconButton } from '@mui/material';
+import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+import { Layout } from '../../components/layouts/Layout';
+import { EntryStatus } from '../../interfaces/';
+import { isValidObjectId } from 'mongoose';
+
+
+const validStatus: EntryStatus[] = [ 'pending', 'in-progress', 'finished' ];
+
+interface Props {
+    id: string;
+};
+
+
+const EntryPage: FC<Props> = () => {
+
+    const [ inputValue, setInputValue ] = useState('');
+    const [ status, setStatus ] = useState<EntryStatus>( 'pending' );
+    const [ touched, setTouched ] = useState( false );
+
+    const isNotValid = useMemo(() => inputValue.length === 0 && touched, [ inputValue, touched ]);
+
+    const onInputChanged = ( event: ChangeEvent<HTMLInputElement> ) => {
+        setInputValue( event.target.value );
+    };
+
+    const onStatusChange = ( event: ChangeEvent<HTMLInputElement> ) => {
+        setStatus( event.target.value as EntryStatus);
+    };
+
+    const onSave = () => {
+        
+    };
+
+    return (
+        <Layout title='...'>
+            <Grid
+                container
+                justifyContent='center'
+                sx={{ marginTop: 2 }}
+            >
+                <Grid
+                    item
+                    xs={ 12 }
+                    sm={ 8 }
+                    md={ 6 }
+                >
+                    <Card>
+
+                        <CardHeader 
+                            title={`Entrada: ${ inputValue }`}
+                            subheader='Creada hace ... minutos'
+                        />
+
+                        <CardContent>
+
+                            <TextField 
+                                sx={{ marginTop: 2, marginBottom: 1 }}
+                                fullWidth
+                                placeholder='Nueva Entrada'
+                                autoFocus
+                                multiline
+                                label='Nueva Entrada'
+                                value={ inputValue }
+                                onBlur={ () => setTouched(true) }
+                                onChange={ onInputChanged }
+                                helperText={ isNotValid && 'Ingrese un valor'}
+                                error={ isNotValid }
+                            />
+
+                            <FormControl>
+
+                                <FormLabel> Estado: </FormLabel>
+
+                                <RadioGroup 
+                                    row 
+                                    onChange={ onStatusChange }
+                                    value={ status }
+                                >
+
+                                    {
+                                        validStatus.map( option => <FormControlLabel key={ option } value={ option } control={ <Radio /> } label={ capitalize(option) } /> )
+                                    }
+
+                                </RadioGroup>
+
+                            </FormControl>
+
+                        </CardContent>
+
+                        <CardActions>
+
+                            <Button
+                                startIcon={ <SaveOutlinedIcon /> }
+                                variant='contained'
+                                fullWidth
+                                onClick={ onSave }
+                                disabled={ inputValue.length === 0 }
+                            >
+                                Save
+                            </Button>
+
+                        </CardActions>
+
+                    </Card>
+
+                </Grid>
+
+            </Grid>
+            
+            <IconButton sx={{
+                position: 'fixed',
+                bottom: 30,
+                right: 30,
+                backgroundColor: 'error.dark'
+            }}
+            >
+
+                <DeleteOutlineOutlinedIcon />
+
+            </IconButton>
+
+        </Layout>
+    );
+};
+
+
+// You should use getServerSideProps when:
+// - Only if you need to pre-render a page whose data must be fetched at request time
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+
+    const { id } = params as { id: string };
+
+    if( !isValidObjectId( id ) ) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            },
+        };
+    };
+
+
+    return {
+        props: {
+            id,
+        },
+    };
+};
+
+
+
+export default EntryPage;
