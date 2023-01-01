c94f985c815436f638c7a24d3ade32c2eee24b4d
Juan Martin Silva <juanmartinsilva74@gmail.com>
2022-12-31 21:43:06 -0300

    shopLayout

--- 05-teslo-shop/pages/index.tsx
+++ 05-teslo-shop/pages/index.tsx
@@ -1,10 +1,18 @@
+import { Typography } from '@mui/material';
 import { NextPage } from 'next'
+import { ShopLayout } from '../components/layouts/';
 
 
 const Home: NextPage = () => {
     
     return (
-        <h1>Teslo shop </h1>
+        <ShopLayout title={'Teslo Shop'} pageDescription={'Encuentra los mejores productos de Teslo aquí!'} >
+
+            <Typography variant='h1' component='h1' > Tienda </Typography>
+            <Typography variant='h2' sx={{ mb: 1 }}> Todos los productos </Typography>
+
+
+        </ShopLayout>
     )
 }
 
