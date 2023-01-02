8ff7c557f404c331fc436804f6be80abcd5ce517
Juan Martin Silva <juanmartinsilva74@gmail.com>
2023-01-02 10:48:21 -0300

    home page temporal

--- 05-teslo-shop/pages/index.tsx
+++ 05-teslo-shop/pages/index.tsx
@@ -1,7 +1,7 @@
-import { Typography } from '@mui/material';
+import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material';
 import { NextPage } from 'next'
 import { ShopLayout } from '../components/layouts/';
-
+import { initialData } from '../database/products';
 
 const Home: NextPage = () => {
     
@@ -11,6 +11,25 @@ const Home: NextPage = () => {
             <Typography variant='h1' component='h1' > Tienda </Typography>
             <Typography variant='h2' sx={{ mb: 1 }}> Todos los productos </Typography>
 
+            <Grid container spacing={ 4 } >
+               
+               {
+                    initialData.products.map( product => (
+                        <Grid item xs={ 6 } sm={ 4 } key={ product.slug } >
+                            <Card>
+                                <CardActionArea>
+                                    <CardMedia 
+                                        component='img'
+                                        image={`products/${ product.images[0] }`}
+                                        alt={ product.title }
+                                    />
+                                </CardActionArea>
+                            </Card>
+                        </Grid>    
+                    ))
+               }
+
+            </Grid>
 
         </ShopLayout>
     )
