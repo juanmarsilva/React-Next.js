16951ae9b60fbf5cc1d4390c9890f2364264ce9f
Juan Martin Silva <juanmartinsilva74@gmail.com>
2023-01-02 11:52:57 -0300

    create productList and product card component

--- 05-teslo-shop/pages/index.tsx
+++ 05-teslo-shop/pages/index.tsx
@@ -1,6 +1,7 @@
-import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 import { NextPage } from 'next'
 import { ShopLayout } from '../components/layouts/';
+import { ProductList } from '../components/products';
 import { initialData } from '../database/products';
 
 const Home: NextPage = () => {
@@ -9,27 +10,12 @@ const Home: NextPage = () => {
         <ShopLayout title={'Teslo Shop'} pageDescription={'Encuentra los mejores productos de Teslo aquí!'} >
 
             <Typography variant='h1' component='h1' > Tienda </Typography>
+            
             <Typography variant='h2' sx={{ mb: 1 }}> Todos los productos </Typography>
 
-            <Grid container spacing={ 4 } >
-               
-               {
-                    initialData.products.map( product => (
-                        <Grid item xs={ 6 } sm={ 4 } key={ product.slug } >
-                            <Card>
-                                <CardActionArea>
-                                    <CardMedia 
-                                        component='img'
-                                        image={`products/${ product.images[0] }`}
-                                        alt={ product.title }
-                                    />
-                                </CardActionArea>
-                            </Card>
-                        </Grid>    
-                    ))
-               }
-
-            </Grid>
+            <ProductList 
+                products={ initialData.products as any } 
+            />
 
         </ShopLayout>
     )
